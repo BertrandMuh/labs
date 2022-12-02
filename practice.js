c7436b4846ed17b17f7a8e1b828e6e344fdb8c93
BertrandMuh <sangwabertrand14@gmail.com>
2022-12-02 00:29:05 -0600

    finish practice.js

--- practice.js
+++ practice.js
@@ -13,19 +13,48 @@ function digitalRoot(n) {
 
 digitalRoot(456)
 
-// var Ball = function (ballType = 'regular') {
-//     // your code goes here
-//     this.ballType = ballType
-// };
-// let ball1 = new Ball();
-// let ball2 = new Ball("super");
-// //console.log(ball2)
+var ball = function (ballType = 'regular') {
+    // your code goes here
+    this.ballType = ballType
+};
+let ball1 = new ball();
+console.log(ball1)
+let ball2 = new ball("super");
+console.log(ball2)
 
-// //console.log(ball2.ballType)
-// String.prototype.toAlternatingCase = function () {
-//     // Define your method here :
-//     let newArray = this.split('')
-//     console
-// }
 
-// 'Hello World'.toAlternatingCase()
+String.prototype.toAlternatingCase = function () {
+    // Define your method here :)
+    let newString = [];
+    //change letter to upper case if lower case
+    for (let i = 0; i < this.length; i++) {
+        if (this[i] === this[i].toLowerCase()) {
+            newString.push(this[i].toUpperCase())
+        }
+        //change letter to lowercase if upper case
+        else {
+            newString.push(this[i].toLowerCase())
+        }
+
+    }
+    return newString.join("")
+}
+
+"hello world".toAlternatingCase()
+"HELLO WORLD".toAlternatingCase()
+"hello WORLD".toAlternatingCase()
+"HeLLo WoRLD".toAlternatingCase()
+"12345".toAlternatingCase()
+"1a2b3c4d5e".toAlternatingCase()
+"String.prototype.toAlternatingCase".toAlternatingCase()
+
+function highAndLow(numbers) {
+    // ...
+    //get the largest value
+    let max = Math.max(...numbers.split(' ').map(x => x));
+    //get the lowest value
+    let min = Math.min(...numbers.split(' ').map(x => x))
+    return `${max} ${min}`;
+}
+let result = highAndLow("1 2 3 4 5");
+console.log(result)
